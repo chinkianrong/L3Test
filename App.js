cfb404a2bf20e091cf05c89b9aca7645d3a592de
Chin Kian Rong <23051124@myrp.edu.sg>
2024-11-28 16:03:39 +0800

    Lesson 3 Code

--- App.js
+++ App.js
@@ -1,10 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View, TextInput, Button, Alert, ToastAndroid, TouchableOpacity} from 'react-native';
+import RNPickerSelect from 'react-native-picker-select';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+
+      <TextInput style={{borderWidth: 1}} />
+
+        <TouchableOpacity onPress={() => Alert.alert("Welcome")} >
+            <Text>LOG IN</Text>
+        </TouchableOpacity>
+
+      <RNPickerSelect
+        onValueChange={(value) => console.log(value)}
+        items={[
+          { label: 'Football', value: 'football' },
+          { label: 'Baseball', value: 'baseball' },
+          { label: 'Hockey', value: 'hockey' },
+        ]}
+      />
+
+        <Button
+            onPress={() => { console.log("Button is pressed") }}
+            title="Press Me"
+        />
+
+        <Button
+            onPress={() => { ToastAndroid.show("Hello World!", ToastAndroid.SHORT)}}
+            title="Log in (less intrusive)"
+        />
+
+        <Button
+            onPress={() => { Alert.alert("Hello World!")}}
+            title="Log in (attention grabbing)"
+        />
+
       <StatusBar style="auto" />
     </View>
   );
